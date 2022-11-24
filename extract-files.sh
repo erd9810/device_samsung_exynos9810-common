9d3f39f99e7824a1ec4dd45db283ae3f747325c5
Dylan Neve <dylanneve1@gmail.com>
2022-11-24 20:34:36 +0000

    exynos9810-common: ril: import N770FXXU8HVK5 stack

--- extract-files.sh
+++ extract-files.sh
@@ -60,9 +60,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/bin/hw/rild)
-            "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
-            ;;
         vendor/lib*/libsensorlistener.so)
             "${PATCHELF}" --add-needed libshim_sensorndkbridge.so "${2}"
             ;;
