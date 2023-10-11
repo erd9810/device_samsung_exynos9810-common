c17fb74527f6438b841daf2afa584a14cd791710
Gabriel Biren <gbiren@google.com>
2023-10-11 13:33:08 +0100

    exynos9810-common: use the AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d

--- common.mk
+++ common.mk
@@ -385,7 +385,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     WifiOverlay \
     wpa_supplicant \
