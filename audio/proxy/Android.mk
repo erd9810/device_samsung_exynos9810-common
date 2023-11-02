a04e06376638b7d79af972cf874fc2706aaf2294
Dylan Neve <dylanneve1@gmail.com>
2023-11-02 17:45:52 +0000

    exynos9810-common: Import libaudioproxy from universal9810 BSP tree
    * Taken from https://gitlab.com/Linaro/96boards/e850-96/platform/hardware/samsung_slsi

---
+++ audio/proxy/Android.mk
@@ -0,0 +1,55 @@
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+SOC_BASE_PATH := $(TOP)/hardware/samsung_slsi/exynos
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	audio_proxy.c
+
+LOCAL_C_INCLUDES += \
+	$(SOC_BASE_PATH)/include/libaudio/audiohal \
+	external/tinyalsa/include \
+	external/tinycompress/include \
+	external/kernel-headers/original/uapi/sound \
+	$(call include-path-for, audio-utils) \
+	$(call include-path-for, audio-route) \
+	$(call include-path-for, alsa-utils) \
+	external/expat/lib
+
+LOCAL_HEADER_LIBRARIES := libhardware_headers
+LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa_sec libtinycompress libaudioutils libaudioroute_sec \
+			  libalsautils_sec libexpat
+
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function
+
+# To use MCD specific definitions
+LOCAL_CFLAGS += -DSUPPORT_MCD_FEATURE
+
+# To use the headers from vndk-ext libs
+LOCAL_CFLAGS += -D__ANDROID_VNDK_SEC__
+
+ifeq ($(BOARD_USE_SOUNDTRIGGER_HAL),true)
+LOCAL_CFLAGS += -DSUPPORT_STHAL_INTERFACE
+LOCAL_CFLAGS += -DTARGET_SOC_NAME=$(TARGET_SOC)
+endif
+
+LOCAL_MODULE := libaudioproxy
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
